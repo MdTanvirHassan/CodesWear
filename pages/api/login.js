a62c6fe3704f0582cb0a54dcb76e1425dffa28d2
Tanvir Hassan <Tanvirtamim72@gmail.com>
2023-03-13 23:39:02 +0600

    Adding Encryption to login and signup.

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,13 +1,17 @@
 import User from "@/models/User";
 import connectDb from "@/middleware/mongoose";
+var CryptoJS = require("crypto-js");
+
 
 const handler = async (req, res) => {
 try {
     if (req.method == "POST") {
       let user = await User.findOne({ "email": req.body.email })
+      const bytes  = CryptoJS.AES.decrypt(user.password, 'secret1234');
+      let decryptedPass = bytes.toString(CryptoJS.enc.Utf8);
 
         if (user) {
-            if (req.body.email == user.email && req.body.password == user.password) {
+            if (req.body.email == user.email && req.body.password == decryptedPass) {
             res.status(200).json({success: true,email: user.email, name: user.name});
             }
             else{
--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,12 +1,15 @@
 import User from "@/models/User";
 import connectDb from "@/middleware/mongoose";
+var CryptoJS = require("crypto-js");
 
 const handler = async (req, res) => {
   try {
     //await connectDb();
-    if (req.method === "POST") {
-      const u = new User(req.body);
+    if (req.method == "POST") {
+      const {name, email} = req.body;
+      const u = new User({name, email, password: CryptoJS.AES.encrypt(req.body.password, 'secret1234').toString()});
       await u.save();
+
       res.status(200).json({ success: "success!" });
     } else {
       res.status(400).json({ error: "error! This method is not allowed." });
